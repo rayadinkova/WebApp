8ce7916b7c17446eca6d975bfbcca0894a81e580
rayadinkova <rayadinkovadesignss@gmail.com>
2023-11-05 14:51:47 +0100

    Working on a Movie carousel with an API

--- WebApp/src/components/Header-404.jsx
+++ WebApp/src/components/Header-404.jsx
@@ -1,11 +1,13 @@
 import "../styles/components_styles/header-404.css";
-import { BiDotsVerticalRounded } from "react-icons/bi";
+import { BiSolidLeftArrow } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 export default function Header404() {
   return (
     <div className="header-404-wrapper">
-      <BiDotsVerticalRounded className="header-404-arrow" />
+      <Link to="/home">
+        <BiSolidLeftArrow className="create-list-header-arrow" />
+      </Link>
     </div>
   );
 }
